3915840ed629c72065ae5e93965a9d6af18d9f25
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2024-04-07 15:25:32 +0000

    Add Education component to Education.jsx

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,6 +4,24 @@ const Education = () => {
   return (
     <section className="light" id="education">
       <h2>Education</h2>
+      <br />
+      <div className="grid">
+        <div className="cell">
+          <h3>University of Colombo School of Computing</h3>
+          <p>Bachelor of Science in Information Systems</p>
+          <p>2022 - 2026</p>
+        </div>
+        <div className="cell">
+          <h3>University of Moratuwa</h3>
+          <p>Information Technology</p>
+          <p>2021 - 2022</p>
+        </div>
+        <div className="cell">
+          <h3>St.Annes Collage</h3>
+          <p>Physical Sciences</p>
+          <p>2017 - 2019</p>
+        </div>
+      </div>
     </section>
   );
 };
